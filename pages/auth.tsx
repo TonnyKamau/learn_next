84bdba92896cbb280dcbab78b727b05b95fa2106
TonnyKamau <tonnykamau6@gmail.com>
2023-03-24 15:45:28 +0300

    auth page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,8 @@
 import Input from "@/components/input";
-
+import { useState } from "react";
 
 const Auth = () => {
+  const [email,setEmail]=useState('');
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -9,17 +10,23 @@ const Auth = () => {
           <img src="/images/logo.png" alt="Logo" className="h-12" />
           <div className="flex justify-center">
             <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-                <h2 className="text-white text-4xl mb-8 font-semibold"> 
-                    Sign in
-                </h2>
-                <div className="flex flex-col gap-4">
-                   <Input /> 
-                </div>
+              <h2 className="text-white text-4xl mb-8 font-semibold">
+                Sign in
+              </h2>
+              <div className="flex flex-col gap-4">
+                <Input
+                  label="Email"
+                  onChange={() => {}}
+                  id="email"
+                  value={email}
+                  type="email"
+                />
+              </div>
             </div>
           </div>
         </nav>
       </div>
     </div>
   );
-}
+};
 export default Auth;
